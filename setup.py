02f9065201aff7cc6ae673d5f39fe9b617ca8f97
hmtu14 <wunjai196@gmail.com>
2023-06-21 11:32:29 +0700

    Remove ignored files - cb & vscode

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='adversarial-test',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.3',
     description='Adversarial test for tabular data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='TuHM',
     install_requires=[
         "numpy",
